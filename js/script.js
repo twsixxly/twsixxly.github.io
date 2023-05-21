9fed9bec211debb0ab93ee669969f5ec9f222f0d
Hasby <hasby.shady@gmail.com>
2023-05-22 03:59:00 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//toglle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik diluar sidebar biar menu ilang
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
